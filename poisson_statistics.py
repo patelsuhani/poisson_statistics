15c9ff3bae7ceb95e0979758d0e53ac63d42688c
patelsuhani <121316408+patelsuhani@users.noreply.github.com>
2024-03-14 05:44:07 -0400

    dividing time into calculated intervals

--- poisson_statistics.py
+++ poisson_statistics.py
@@ -8,3 +8,12 @@ def find_min_max_intervals(filename, num_intervals):
     # Find minimum and maximum time moments
     min_time = np.min(data)
     max_time = np.max(data)
+
+    # Calculate interval size
+    interval_size = (max_time - min_time) / num_intervals
+
+    # Break time into equal intervals
+    intervals = np.linspace(min_time, max_time, num_intervals+1)
+
+    return min_time, max_time, intervals
+
